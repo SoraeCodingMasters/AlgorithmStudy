c7a057e3bc750e8045907a66d4e9bf6be53ee451
kimnamju <kimnamju0920@gmail.com>
2024-02-07 17:16:10 +0900

    9017-2

--- 김남주/9017_크로스컨트리.cpp
+++ 김남주/9017_크로스컨트리.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int t,n;
-int a[1001],num[201],score[201],fif[201],cur_num[201];
+int a[1001],num[201],score[201],cur_num[201];
 
 int main() {
     fastio;
@@ -22,31 +22,23 @@ int main() {
             t_n=max(t_n,a[i]);  
             num[a[i]]++;
         }
-        for (int i=1;i<=t_n;i++) score[i]=0,fif[i]=0,cur_num[i]=0;
+        for (int i=1;i<=t_n;i++) score[i]=0,cur_num[i]=0;
         int x=0;
+        int min_score=1e9,min_fif=1e9;
+        int ans;
         for (int i=1;i<=n;i++) {
             if (num[a[i]]<6) continue;
             ++x;
             if (++cur_num[a[i]]<=4) score[a[i]]+=x;
-            if (cur_num[a[i]]==5) fif[a[i]]=x;
-        }
-
-        int min_score=1e9;
-        for (int i=1;i<=t_n;i++) {
-            if (num[i]<6) continue;
-            if (min_score<score[i]) {
-                min_score=score[i];
-            }
-            min_score=min(min_score,score[i]);
-            
-        }
-        int ans=0;
-        int min_fif=1e9;
-        for (int i=1;i<=t_n;i++) {
-            if (score[i]==min_score&&num[i]==6) {
-                if (fif[i]<min_fif) {
-                    min_fif=fif[i];
-                    ans=i;
+            if (cur_num[a[i]]==4) {
+                if (min_score>score[a[i]]) {
+                    min_score=score[a[i]];
+                    ans=a[i];
+                }
+            } else if (cur_num[a[i]]==5) {
+                if (min_score==score[a[i]] && min_fif>x) {
+                    min_fif=x;
+                    ans=a[i];
                 }
             }
         }
