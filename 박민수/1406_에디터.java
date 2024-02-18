935b0861bda510883104f051471ea0fbe2dba202
pmsu2007 <pmsu2008@gmail.com>
2024-02-18 16:05:57 +0900

    1406 solved

--- 박민수/1406_에디터.java
+++ 박민수/1406_에디터.java
@@ -3,8 +3,10 @@ package SoraeCodingMasters.BOJ1406;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -17,32 +19,37 @@ import java.util.StringTokenizer;
  */
 
 public class Main {
-    static final int MAX_LEN = 600_000;
     static String input;  // 1 <= len(input) <= 100,000
     static int M; // 1 <= M <= 500,000
     static List<Character> editor = new LinkedList<>();
     static char command;
-    static int cursor;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         input = br.readLine();
-        cursor = input.length();
         for (char c : input.toCharArray()) {
             editor.add(c);
         }
 
         M = Integer.parseInt(br.readLine());
 
-        while(M-- > 0) {
-            String s = br.readLine();
-            command = s.charAt(0);
+        ListIterator<Character> iterator = editor.listIterator(); // Linked List의 cursor
+        for (char c : editor) {
+            iterator.next(); // 맨 끝으로 옮기기
+        }
 
-            if (command == 'L' && cursor > 0) cursor--;
-            else if (command == 'D' && cursor < editor.size()) cursor++;
-            else if (command == 'B' && cursor > 0) editor.remove(--cursor);
-            else if (command == 'P') editor.add(cursor++, s.charAt(2));
+        while(M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            command = st.nextToken().charAt(0);
+
+            if (command == 'L' && iterator.hasPrevious()) iterator.previous();
+            else if (command == 'D' && iterator.hasNext()) iterator.next();
+            else if (command == 'B' && iterator.hasPrevious()) {
+                iterator.previous();
+                iterator.remove();
+            }
+            else if (command == 'P') iterator.add(st.nextToken().charAt(0));
         }
 
         StringBuilder sb = new StringBuilder();
