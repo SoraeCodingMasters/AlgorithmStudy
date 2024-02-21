11b6610901f72b7dabc75859b040d702acd2a363
Namju Kim <55823958+calculus12@users.noreply.github.com>
2024-02-21 14:22:23 +0900

    14940

---
+++ 김남주/14940_쉬운최단거리.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+#define fastio cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
+#define read_input freopen("input.txt","r",stdin)
+using namespace std;
+
+typedef pair<int, int> pii;
+#define MAX 1000
+int n, m;
+int g[MAX][MAX];
+int dist[MAX][MAX];
+int dy[4]{ 0,0,1,-1 }, dx[4]{ 1,-1,0,0 };
+const int INF = 987654321;
+int main() {
+	fastio;
+	//read_input;
+	cin >> n >> m;
+	int sy,sx;
+	for (int i = 0;i < n;i++) for (int j = 0;j < m;j++) {
+		cin >> g[i][j];
+		if (g[i][j] == 2) sy = i, sx = j;
+	}
+	queue<pii> q;
+	dist[sy][sx] = 1;
+	q.push({sy,sx});
+	while (!q.empty()) {
+		auto [cy, cx] = q.front();
+		q.pop();
+
+		for (int d = 0;d < 4;d++) {
+			int ny = cy + dy[d], nx = cx + dx[d];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m || g[ny][nx]==0 || dist[ny][nx]>0) continue;
+			dist[ny][nx] = dist[cy][cx] + 1;
+			q.push({ ny,nx });
+		}
+	}
+	for (int i = 0;i < n;i++) {
+		for (int j = 0;j < m;j++) {
+			if (!dist[i][j]) cout << (g[i][j] == 0 ? 0 : -1) << ' ';
+			else cout << dist[i][j]-1 << ' ';
+		}
+		cout << '\n';
+	}
+}
