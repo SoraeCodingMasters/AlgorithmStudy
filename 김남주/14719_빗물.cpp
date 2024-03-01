c952fc0477abb2ea93dbb53cd1d66943e198a311
Namju Kim <55823958+calculus12@users.noreply.github.com>
2024-03-02 00:27:30 +0900

    14719-bfs

--- 김남주/14719_빗물.cpp
+++ 김남주/14719_빗물.cpp
@@ -1,37 +1,32 @@
 #include <iostream>
-#include <stack>
+#include <queue>
 
 #define fastio cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
 using namespace std;
 
-int h,w,x[500];
+int h,w,x[500],target[500];
 
 int main() {
     fastio;
     cin>>h>>w;
-    stack<pair<int,int>> stk;
-    int cur,ans=0;
-    int prev;
-    int s,k;
     for (int i=0;i<w;i++) {
-        cin>>cur;
-        s=i;
-        k=0;
-        while(!stk.empty() && stk.top().first<=cur) {
-            k=max(k,stk.top().first);
-            s=stk.top().second;
-            stk.pop();
-        }
-        if (!stk.empty()) {
-            k=cur;
-            s=stk.top().second+1;
-        }
-        for (int j=i-1;j>=s;j--) {
-            ans+=k-x[j];
-            x[j]=k;
-        }
-        x[i]=cur;
-        stk.push({cur,i});
+        cin>>x[i];
+        target[i]=501;
     }
+    target[0]=x[0],target[w-1]=x[w-1];
+    queue<int> q;
+    q.push(0),q.push(w-1);
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        for (int i=-1;i<=1;i+=2) {
+            if (cur+i<0 || cur+i>=w) continue;
+            if (target[cur+i]<=target[cur]) continue;
+            target[cur+i]=max(target[cur],x[cur+i]);
+            q.push(cur+i);
+        }
+    }
+    int ans=0;
+    for (int i=0;i<w;i++) ans+=(target[i]-x[i]);
     cout<<ans;
 }
