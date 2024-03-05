3843802fcf355eeeac47496e297a959d0be95bf8
Namju Kim <55823958+calculus12@users.noreply.github.com>
2024-03-05 12:41:03 +0900

    7682-2

--- 김남주/7682_틱택토.cpp
+++ 김남주/7682_틱택토.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
 #define fastio cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
 #define read_input freopen("input.txt","r",stdin)
 using namespace std;
 
-string s;
-
+string s=".........";
+bool possible[20000];
+char l[2] ={'X','O'};
 bool check(char cur) {
     bool f=false;
     for (int r=0;r<3;r++) {
         f=true;
         for (int c=0;c<3;c++) {
-            if (s[3*r+c]!=cur) {
+            if (s[r*3+c]!=cur) {
                 f=false;
                 break;
             }
         }
         if (f) return true;
     }
+
     for (int c=0;c<3;c++) {
         f=true;
         for (int r=0;r<3;r++) {
@@ -29,43 +32,53 @@ bool check(char cur) {
         }
         if (f) return true;
     }
-    if (s[0]==cur && s[4]==cur && s[8]==cur) return true;
-    if (s[2]==cur && s[4]==cur && s[6]==cur) return true;
+    if (s[0]==cur&&s[4]==cur&&s[8]==cur) return true;
+    if (s[2]==cur&&s[4]==cur&&s[6]==cur) return true;
     return false;
 }
-
-pair<bool,bool> x_o_f() {
-    bool x_f=check('X');
-    bool o_f=check('O');
-    return {x_f,o_f};
+void bf(int num, int b, int c) {
+    if (num==9) {
+        possible[b]=true;
+        return;
+    }
+    if (check('X')) {
+        possible[b]=true;
+        return;
+    }
+    if (check('O')) {
+        possible[b]=true;
+        return;
+    }
+    int x=1;
+    for (int i=0;i<9;i++) {
+        if (s[i]=='.') {
+            s[i]=l[c];
+            bf(num+1,b+x*(c?1:2),c?0:1);
+            s[i]='.';
+        }
+        x*=3;
+    }
 }
+
 int main() {
     fastio;
     // read_input;
+    bf(0,0,0);
+    string input;
     while(1) {
-        cin>>s;
-        if (s=="end")break;
-        int x_n=0,o_n=0;
-        for (int i=0;i<9;i++)
-            if (s[i]=='O') o_n++;
-            else if (s[i]=='X') x_n++;
-        if (!(x_n-o_n==1 || x_n==o_n)) {
-            cout<<"invalid"<<'\n';
-            continue;
+        cin>>input;
+        if (input.front()=='e') break;
+        int idx=0;
+        int x=1;
+        for (int i=0;i<9;i++) {
+            if (input[i]=='X') {
+                idx+=x*2;
+            } else if (input[i]=='O') {
+                idx+=x;
+            }
+            x*=3;
         }
-        auto [x_f,o_f]=x_o_f();
-        if (x_f && x_n-o_n!=1) {
-            cout<<"invalid"<<'\n';
-            continue;
-        }
-        if (o_f && x_n!=o_n) {
-            cout<<"invalid"<<'\n';
-            continue;
-        }
-        if (!x_f&&!o_f&&x_n+o_n!=9) {
-            cout<<"invalid"<<'\n';
-            continue;
-        }
-        cout<<"valid"<<'\n';
+        if (possible[idx]) cout<<"valid\n";
+        else cout<<"invalid\n";
     }
 }
