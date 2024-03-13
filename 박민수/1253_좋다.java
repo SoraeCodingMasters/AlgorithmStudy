54b7cee19ea01c306ec9b7cb518cd8daffe2b997
pmsu2007 <pmsu2007@naver.com>
2024-03-13 17:43:52 +0900

    1253 : solved binary search

--- 박민수/1253_좋다.java
+++ 박민수/1253_좋다.java
@@ -4,19 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /***
  * 백준 1253번
- * 좋다
+ * 좋다 (Binary Search) 버전
  * 2024-03-13
  * 시간 제한 : 2초
  * 메모리 제한 : 256MB
  */
 
-public class Main {
+public class BS {
     static int N; // 1 <= N <= 2,000
     static int[] arr;
     static int ans = 0;
@@ -24,34 +22,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-
         arr = new int[N];
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        for (int i = 0; i < N; i++) {
-            int l = i == 0 ? 1 : 0;
-            int r = i == N - 1 ? N - 2 : N - 1;
+        for(int i = 0; i < N; i++) {
             int target = arr[i];
-            while(l < r) {
-                int sum = arr[l] + arr[r];
+            boolean flag = false;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
 
-                if (sum > target) {
-                    r = r - 1 == i ? r - 2 : r - 1;
-                } else if (sum < target) {
-                    l = l + 1 == i ? l + 2 : l + 1;
-                } else {
-                    ans++;
-                    break;
+                int l = j + 1;
+                int r = N - 1;
+
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+                    int second = target - arr[j];
+
+                    if (second < arr[mid]) {
+                        r = mid - 1;
+                    } else if (second == arr[mid] && j != mid && i != mid) {
+                        flag = true;
+                        break;
+                    } else {
+                        l = mid + 1;
+                    }
                 }
             }
+            if (flag) ans++;
         }
 
         System.out.println(ans);
     }
-
 }
